886a6cb1f73dae15053212927a60eb0d95066760
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-03 02:50:54 +0300

    mac+

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,3 +1,5 @@
+// npm test ./test/mac-address.test.js
+
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
@@ -14,9 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let string = n.split('-').join('');
+  string = string.replace(/[0-9A-Fa-f]/g, '');
+  return string.length === 0;
 }
 module.exports = {
   isMAC48Address
